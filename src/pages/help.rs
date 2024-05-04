b99732e531a3378094e481fa9ee8ed37b21a161d
ChloeWKY <chloekeyiw@gmail.com>
2024-05-04 17:19:03 -0400

    Add icon to the help section

--- src/pages/help.rs
+++ src/pages/help.rs
@@ -1,4 +1,5 @@
 use yew::{function_component, html, Html};
+use yew_icons::{Icon, IconId};
 
 use crate::components::head_bar::HeadBar;
 
@@ -11,35 +12,54 @@ pub fn help() -> Html {
                 <h1 class="text-center text-6xl m-0">{ t!("help.title")}</h1>
                 <br />
 
-                <h3 class="text-left text-2xl m-0">{ t!("help.app_icon_title")}</h3>
+                <div class="flex items-center">
+                    <h3 class="text-left text-2xl m-0">{ t!("help.app_icon_title")}</h3>
+                    <Icon icon_id={IconId::FeatherPaperclip} class="mx-2 mt-0.5"/>
+                </div>
                 <div class="mx-3 my-2 text-2x">{ t!("help.app_icon")}</div>
                 <br />
 
-                <h3 class="text-left text-2xl m-0">{ t!("help.open_menu_tray_title")}</h3>
+                <div class="flex items-center">
+                    <h3 class="text-left text-2xl m-0">{ t!("help.open_menu_tray_title")}</h3>
+                </div>
                 <div class="mx-3 my-2 text-2x">{ t!("help.open_menu_tray")}</div>
                 <br />
 
-                <h3 class="text-left text-2xl m-0">{ t!("help.open_window_title")}</h3>
+                <div class="flex items-center">
+                    <h3 class="text-left text-2xl m-0">{ t!("help.open_window_title")}</h3>
+                </div>
                 <div class="mx-3 my-2 text-2x">{ t!("help.open_window")}</div>
                 <br />
 
-                <h3 class="text-left text-2xl m-0">{ t!("help.pause_monitoring_title")}</h3>
+                <div class="flex items-center">
+                    <h3 class="text-left text-2xl m-0">{ t!("help.pause_monitoring_title")}</h3>
+                </div>
                 <div class="mx-3 my-2 text-2x">{ t!("help.pause_monitoring")}</div>
                 <br />
 
-                <h3 class="text-left text-2xl m-0">{ t!("help.search_title")}</h3>
+                <div class="flex items-center">
+                    <h3 class="text-left text-2xl m-0">{ t!("help.search_title")}</h3>
+                </div>
                 <div class="mx-3 my-2 text-2x">{ t!("help.search")}</div>
                 <br />
 
-                <h3 class="text-left text-2xl m-0">{ t!("help.pinned_clips_title")}</h3>
+                <div class="flex items-center">
+                    <h3 class="text-left text-2xl m-0">{ t!("help.pinned_clips_title")}</h3>
+                    <Icon icon_id={IconId::BootstrapPinAngle} class="mx-2 mt-0.5"/>
+                </div>
                 <div class="mx-3 my-2 text-2x">{ t!("help.pinned_clips")}</div>
                 <br />
 
-                <h3 class="text-left text-2xl m-0">{ t!("help.favourites_title")}</h3>
+                <div class="flex items-center">
+                    <h3 class="text-left text-2xl m-0">{ t!("help.favourites_title")}</h3>
+                    <Icon icon_id={IconId::BootstrapHeart} class="mx-2 mt-0.5"/>
+                </div>
                 <div class="mx-3 my-2 text-2x">{ t!("help.favourites")}</div>
                 <br />
 
-                <h3 class="text-left text-2xl m-0">{ t!("help.auto_delete_duplications_title")}</h3>
+                <div class="flex items-center">
+                    <h3 class="text-left text-2xl m-0">{ t!("help.auto_delete_duplications_title")}</h3>
+                </div>
                 <div class="mx-3 my-2 text-2x">{ t!("help.auto_delete_duplications")}</div>
             </div>
         </div>

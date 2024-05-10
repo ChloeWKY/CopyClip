60a23745b7fd393c574077eda202f4619676d0f0
Chloe <118044032+ChloeWKY@users.noreply.github.com>
2024-05-10 01:32:20 -0400

    Revert "Revert "Revert "Update branch from upstream branch"""

--- src-tauri/src/clip/clip_struct.rs
+++ src-tauri/src/clip/clip_struct.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 
 use serde::{Deserialize, Serialize};
-use tauri::{AppHandle, Manager};
+use tauri::{AppHandle, ClipboardManager};
 
 use crate::error::Error;
 
@@ -43,21 +43,23 @@ where
 impl Clip {
     /// copy the clip to the clipboard
     pub fn copy_clip_to_clipboard(&self, app: &AppHandle) -> Result<(), Error> {
-        let clipboard_manager = app.state::<tauri_plugin_clipboard::ClipboardManager>();
-        match clipboard_manager.write_text((*self.text).clone()) {
-            Ok(_) => Ok(()),
-            Err(e) => Err(Error::WriteToSystemClipboardErr(
+        let mut clipboard_manager = app.clipboard_manager();
+        let res = clipboard_manager.write_text((*self.text).clone());
+        if let Err(e) = res {
+            return Err(Error::WriteToSystemClipboardErr(
                 (*self.text).clone(),
                 e.to_string(),
-            )),
+            ));
         }
+        Ok(())
     }
 
     /// convert to json format string
     pub fn to_json_string(&self) -> Result<String, Error> {
-        match serde_json::to_string(self) {
-            Ok(s) => Ok(s),
-            Err(e) => Err(Error::ExportError(e.to_string())),
+        let res = serde_json::to_string(self);
+        if let Err(e) = res {
+            return Err(Error::ExportError(e.to_string()));
         }
+        Ok(res.unwrap())
     }
 }
